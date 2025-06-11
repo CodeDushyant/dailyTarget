3152309328e3bbee49c3891b1896322e68d2854f
Dushyant Singh Chauhan <dushyantsingh5161@gmail.com>
2025-06-11 16:29:39 +0530

    connected MongoDB Atlas and updated server.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:5000/api';
+const API_BASE = 'https://dailytarget-b2qy.onrender.com';
 const container = document.getElementById('timeSlotsContainer');
 const saveBtn = document.getElementById('saveBtn');
 const historyContainer = document.getElementById('historyContainer');
